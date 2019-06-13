569976f250539426249e6354bf1502eb167ed92c
barrezuetai <barrezuetai@allegheny.edu>
2019-06-13 13:48:33 -0400

    updated scraper

--- src/twitterScrapper.py
+++ src/twitterScrapper.py
@@ -1,22 +1,51 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import sqlite3
+import datetime
+import time
 
-consumer_key = "2kSzNO1KCKJBIFVa1Qa5q6CGz"
-consumer_secret = "WG60uZKAj6eMVmW2U5nPtegO8scQQB8IcftKY4qhfEZgP0PVVN"
-access_token = "1117533082968571904-iXUTQITNuRlde9xT08Mb18O4RyjqD7"
-access_token_secret = "V8KweWjzflKZGAWPLLbjArheBO2F4DbeldkQ5zX6mgDVm"
+consumer_key = 'consumer_key'
+consumer_secret = `consumer_secret`
+access_token = `access_token`
+access_token_secret = 'access_token_secret'
+
+def insertTweets(user, text, time, location, retweeted_status, conn):
+    hashtag="tbd"
+    InsertIntoTweet = "INSERT INTO Tweets(username, tweetLocation, tweetTime, tweetText, hashtag, isRetweet) VALUES(\"{B}\",\"{C}\",\"{D}\",\"{E}\",\"{F}\",\"{G}\");".format(B = user, C = location, D = time, E = text, F = hashtag, G = retweeted_status)
+    try:
+        result = conn.execute(InsertIntoTweet)
+        conn.commit()
+    except:
+        print("err")
 
 class MyStreamListener(StreamListener):
-
-    def on_status(self, status):
-        print(status.text)
+    conn = sqlite3.connect('twitterDB.sqlite3')
+    def on_status(self, status, conn = sqlite3.connect('twitterDB.sqlite3')):
+        print("created at: " + str(status.created_at))
+        print("text: " + status.text)
+        isRetweet = False
+        try:
+            if status.retweeted_status:
+                isRetweet = True
+        except:
+            isRetweet = False
+        insertTweets(status.user.screen_name, status.text, status.created_at, status.user.location, isRetweet, conn)
 
 l = MyStreamListener()
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-myStreamListener = MyStreamListener()
-myStream = Stream(auth, l)
+while(True):
+    try:
+        myStreamListener = MyStreamListener()
+        myStream = Stream(auth, l)
 
-myStream.filter(track=['avengers'])
+        myStream.filter(track=['avengers', 'gameofthrones'])
+    except:
+        print("You probably got kicked out, wait a few minutes")
+        f = open("errorlog.txt", "a")
+        currentDT = datetime.datetime.now()
+        f.write(str(currentDT))
+        f.close()
+        time.sleep(1300)
